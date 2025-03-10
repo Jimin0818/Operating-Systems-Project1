ff87e57b6576f2d0c405cc1c9527c40ba703ec0a
Ji Min Yoon <151479951+Jimin0818@users.noreply.github.com>
2025-03-09 19:53:34 -0500

    Update driver.py
    Initial starting part of the driver program

--- driver.py
+++ driver.py
@@ -1,1 +1,26 @@
+import sys
+import subprocess
 
+
+def main():
+    if len(sys.argv) != 2:
+        print("python driver.py <logfile>")
+        sys.exit(1)
+
+
+    logging_file = sys.argv[1]
+
+
+    logger = subprocess.Popen(["python", "logger.py", logging_file], stdin=subprocess.PIPE, encoding="utf8")
+    encryptor = subprocess.Popen(["python", "encrypt.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding="utf8")
+
+
+    history = []
+
+
+    def log(action, message=""):
+        logger.stdin.write(f"{action} {message}\n")
+        logger.stdin.flush()
+
+
+    log("START", "Driver started")
